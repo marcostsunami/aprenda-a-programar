4e2e82f264a1890916543bdcb37b5f156ed94f9e
MARCOS AURELIO RIBEIRO <marcostsunami@gmail.com>
2022-08-24 20:48:16 -0300

    Commit da aula 3

---
+++ aula3.js
@@ -0,0 +1,15 @@
+let name ="Marcolino"
+let idade =10
+let idade2 =30
+
+if (idade >=18){
+    console.log (name + " Pode entra na página.")
+}else{
+    console.log(name + " não pode entrar");
+}
+
+if (idade2 >=18){
+    console.log (`${name}  Pode entra na página.`);
+}else{
+    console.log(`${name}  não pode entrar'`);
+}
--- helloWord.js
+++ helloWord.js
@@ -1,4 +1,9 @@
 // comentário
 let variavel
+let idade
 variavel ="ITuring"
+idade = 6;
 console.log(variavel);
+if (idade >= 18){
+    console.log("Pode entrar!");
+ }else{ console.log("Não pode entra.");}
