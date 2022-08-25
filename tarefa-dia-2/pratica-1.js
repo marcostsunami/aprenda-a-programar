c76867acb5704b03e10b521700e9bc1105bcc630
MARCOS AURELIO RIBEIRO <marcostsunami@gmail.com>
2022-08-25 17:32:01 -0300

    Commit colocando mensagem de viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,5 @@ let varEmail="marcostsunami@gmail.com"
 let varProfissao="Analista"
 let VarIdade=37
 let varSonho="México"
+
+console.log (`Olá, ${varNome} ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${varSonho}. Confirma pra gente alguns dados? A sua idade é ${VarIdade} , você é ${varProfissao} e o seu email é ${varEmail} ? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
